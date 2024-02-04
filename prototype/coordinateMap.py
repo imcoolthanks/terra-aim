eca63f3368093e3e4844be3292a26542b85c3d22
imcoolthanks <wongq9999@outlook.com>
2024-02-04 08:09:15 +0000

    Reset database

--- prototype/coordinateMap.py
+++ prototype/coordinateMap.py
@@ -1,38 +1,22 @@
 from math import tan, inf
 
 class CoordinateMap:
-    def __init__(self, calibration_coordinates, distance: int):
+    def __init__(self, distance, screen_width, screen_height):
 
         # TODO: make sure that increase yaw and pitch correlate to increase in x and y
         
         # calibration_coordinates = [("x", "y", "yaw", "pitch"), ...]
         self.distance = distance
-
-        #     -----------------------(x2, y2)
-        #    |                           |
-        #    |                           |
-        #    |                           |
-        #    |                           |
-        # (x1, y1)----------------------- 
-
-
-        self.x1, self.y1, self.origin_yaw, self.origin_pitch = calibration_coordinates[0]
-        
-        try:
-            self.x2 = calibration_coordinates[1][0] + self.distance / tan(calibration_coordinates[1][2] - self.origin_yaw)
-            self.y2 = calibration_coordinates[1][1] + self.distance / tan(calibration_coordinates[1][3] - self.origin_pitch)
-        except (ZeroDivisionError, ValueError):
-            self.x2 = 100
-            self.y2 = 100
+        self.screen_width = screen_width
+        self.screen_height = screen_height
 
     def get_position(self, x, y, yaw, pitch):
-
         try:
-            (projected_x, projected_y) = (x + self.distance / tan(yaw - self.origin_yaw), y + self.distance / tan(pitch - self.origin_pitch))
+            projected_x = self.screen_width / 2 + self.distance / tan(yaw)
+            projected_y = self.screen_height / 2 + self.distance / tan(pitch)
 
             print(f"projected_x: {projected_x}, projected_y: {projected_y}")
             
-            return ((projected_x - self.x1) / (self.x2 - self.x1),
-                    (projected_y - self.y1) / (self.y2 - self.y1))
+            return (projected_x / self.screen_width, projected_y / self.screen_height)
         except (ZeroDivisionError, ValueError):
             return (0.5, 0.5)
