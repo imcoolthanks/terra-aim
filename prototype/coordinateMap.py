54087e0350673f0f592b8843f348354cfca0fe86
imcoolthanks <wongq9999@outlook.com>
2024-02-04 10:17:10 +0000

    sdg

--- prototype/coordinateMap.py
+++ prototype/coordinateMap.py
@@ -29,6 +29,6 @@ class CoordinateMap:
 
             print(f"projected_x: {projected_x}, projected_y: {projected_y}")
             
-            return (projected_x / self.screen_width, projected_y / self.screen_height)
+            return (projected_x / 100, projected_y / 56)
         except (ZeroDivisionError, ValueError):
             return (0.5, 0.5)
